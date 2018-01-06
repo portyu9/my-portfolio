a296c8cfd24a122a1b7be889702067e6245f780a
Yunior Portal <yunior.portal@gmail.com>
2018-01-06 18:25:23 -0500

    Update latest python script for Portfolio Deployent

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -8,14 +8,27 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:002366342434:deployPortfolioTopic')
 
+    location = {
+        "bucketName": 'portfoliobuild.yunior-portal.com',
+        "objectKey": 'portfoliobuild.zip'
+    }
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact ["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print "Building portfolio from " + str(location)
+
         s3= boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-        portfolio_bucket= s3.Bucket('portfolio.yunior-portal.com')
-        build_bucket= s3.Bucket('portfoliobuild.yunior-portal.com')
+        portfolio_bucket= s3.Bucket(location["bucketName"])
+        build_bucket= s3.Bucket(location["bucketName"])
 
         portfolio_zip= StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -23,7 +36,11 @@ def lambda_handler(event, context):
                 portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
+        print "Job Completed"
         topic.publish(Subject="Portfolio Deployment", Message="Portfolio Deployed Successfully")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId==job[id])
     except:
        topic.publish(Subject="Portfolio Deployment", Message="Portfolio Deployment Failed")
        raise
