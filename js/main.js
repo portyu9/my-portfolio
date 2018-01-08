9cc148796ae448f855de8f5fadb96d315d027207
Yunior Portal <yunior.portal@gmail.com>
2018-01-07 22:43:07 -0500

    added tests for bubble menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+    'title': "AWS Certified",
+    'image': {
+      'desc':"description of work 1",
+      'src': "images/example1.png",
+      'comment':""
+    }
+  },
+  {
+    'title': `I
+    ♥
+    AWS!`,
+    'image': {
+      'desc':"description of work 2",
+      'src': "images/example2.png",
+      'comment':""
+    }
+  },
+  {
+    'title': `Web
+    UI
+    Automation`,
+    'image': {
+      'desc':"description of work 3",
+      'src': "images/example3.png",
+      'comment':""
+    }
+  }
+]
 
-console.log("WebPack works!");
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
